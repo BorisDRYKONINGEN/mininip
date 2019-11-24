93e2c183560f5aa59998354e4ac3cc3dff4a9fdc
BorisD <boris.d@orange.fr>
2019-11-24 15:12:38 +0100

    Module `dump` created and `dump_str` moved into it. Since I forgot to cancel a modification, this commit is not usable as it, but the next version should fix it

---
+++ src/dump/mod.rs
@@ -0,0 +1,54 @@
+//! Provides tools to generate a INI file from any data
+
+/// Formats a `&str` by escaping special characters
+/// 
+/// # Return value
+/// A `String` containing the escaped string
+/// 
+/// # Why should I format it?
+/// The `Display` trait is about displaying a value to the user while `Debug` is for debuging. There is not any trait for dumping a value in a file knowing it can't be backed up in the same way it is displayed, so `escape` does this.
+/// 
+/// For instance, if `content` is `"a'bc=123;"`, then, `escape` will return `r"a\'bc\=123\;"` because it escapes special characters such as `=`, `'`, `;`, ...
+/// 
+/// More escaped characters may be found at [Wikipedia](https://en.wikipedia.org/wiki/INI_file#Escape_characters "INI file")
+/// 
+/// # Examples
+/// ```
+/// use mininip::dump::dump_str;
+/// 
+/// assert_eq!(dump_str("a'bc=123;"), r"a\'bc\=123\;");
+/// ```
+pub fn dump_str(content: &str) -> String {
+    let mut new = String::with_capacity(content.len());
+
+    for i in content.chars() {
+        match i {
+            // Those characters have a special rule to be escaped
+            '\\'   => new.push_str(r"\\"),
+            '\''   => new.push_str("\\'"),
+            '"'    => new.push_str("\\\""),
+            '\0'   => new.push_str("\\0"),
+            '\x07' => new.push_str("\\a"),
+            '\x08' => new.push_str("\\b"),
+            '\t'   => new.push_str("\\t"),
+            '\r'   => new.push_str("\\r"),
+            '\n'   => new.push_str("\\n"),
+            ';'    => new.push_str("\\;"),
+            '#'    => new.push_str("\\#"),
+            '='    => new.push_str("\\="),
+            ':'    => new.push_str("\\:"),
+
+            // The ASCII characters are left unchanged
+            _ if i.is_ascii() => new.push_str(&format!("{}", i)),
+
+            // The non-ASCII characters are escaped with `\x??????`
+            _ => new.push_str(&format!("\\x{:06x}", i as u32)),
+        }
+    }
+
+    new
+}
+
+
+#[cfg(test)]
+mod tests;
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 //! An minimalist ini file parser (MinIniP stands for Minimalist Ini Parser). It is written in Rust but I would export its API to the C programming language in order to make various bindings
 
 pub mod datas;
+pub mod dump;
+pub mod parse;
 
