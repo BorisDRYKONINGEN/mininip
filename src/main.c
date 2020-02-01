afa3c86899d60edf59f5d34906971bdb6a144f00
Boris DRYKONINGEN <boris.d@orange.fr>
2020-02-01 14:09:33 +0100

    mininip.h updated and demo (main.c uses these new features)

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 #include "mininip.h"
 
-int main(void) {
+int main(int argc, const char* const* argv) {
     MininipParser* parser = mininipNewParser();
     printf("%p\n", parser);
 
@@ -10,5 +10,26 @@ int main(void) {
     printf("%p\n", data);
 
     mininipDestroyParserData(data);
+
+    if (argc <= 1)
+        return 0;
+
+    // From now, we know argc > 1 and so, argv[1] is valid
+    MininipData* fileDatas = NULL;
+    MininipError err = mininipParseFile(argv[1], &fileDatas);
+    if (err.kind != MININIP_NO_ERROR) {
+        fprintf(stderr, "An error has occured : 0x%04x ", err.kind);
+        if (err.msg)
+            fputs(err.msg, stderr);
+        else
+            fputs("NULL", stderr);
+        fputc('\n', stderr);
+
+        return -1;
+    }
+
+    printf("%p\n", fileDatas);
+    mininipDestroyParserData(fileDatas);
+
     return 0;
 }
--- src/mininip.h
+++ src/mininip.h
@@ -8,10 +8,23 @@ typedef struct MininipParser MininipParser;
 struct MininipData;
 typedef struct MininipData MininipData;
 
+typedef enum MininipErrorKind {
+    MININIP_NO_ERROR = 0,
+    MININIP_PARSE_ERROR,
+    MININIP_IO_ERROR,
+    MININIP_RUNTIME_ERROR,
+} MininipErrorKind;
+
+typedef struct MininipError {
+    const char* msg;
+    MininipErrorKind kind;
+} MininipError;
+
 MininipParser* mininipNewParser(void);
 void mininipDestroyParser(MininipParser*);
 MininipData* mininipGetParserData(MininipParser*);
 void mininipDestroyParserData(MininipData*);
+MininipError mininipParseFile(const char* path, MininipData**);
 
 #ifdef __cplusplus
 }
